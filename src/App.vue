ffdb8992a4d5907959b61ec3e288f6f84e11178b
apple <apple@appledeiMac.local>
2019-10-31 17:54:19 +0800

    第一次提交“

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,25 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#" class="brand-logo">Logo</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="sass.html">Sass</a></li>
+          <li><a href="badges.html">Components</a></li>
+          <li><a href="collapsible.html">JavaScript</a></li>
+        </ul>
+      </div>
+    </nav>
+
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import HelloWorld from './components/HelloWorld.vue'
+import './assets/js/materialize'
+import './assets/sass/materialize.scss'
 
 @Component({
   components: {
